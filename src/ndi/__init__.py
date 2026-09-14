37379cd7d40396fff4b3b08105428f841b9b0e57
Andrii <bigun.andrey@gmail.com>
2026-09-14 21:29:46 +0300

    feat: export adapter output validation

--- src/ndi/__init__.py
+++ src/ndi/__init__.py
@@ -5,7 +5,7 @@ from .matching import compare_documents, match_nodes, normalize_text
 from .observations import RawObservation, add_observation, observation_discrepancies, parser_coverage
 from .observation_artifacts import observation_artifact_json, observation_artifact_sha256, observation_manifest
 from .adapters import from_docling_records, from_markitdown, from_opendataloader_records, from_pypdf_pages, from_records, new_document
-from .adapter_contract import AdapterCapabilities, ParserAdapter, validate_adapter_capabilities
+from .adapter_contract import AdapterCapabilities, ParserAdapter, validate_adapter_capabilities, validate_adapter_output
 from .adapter_registry import RegisteredAdapter, adapt_all, adapt_registered, default_adapters, validate_default_adapters
 from .ingestion import ObservationArtifact, ObservationPackage, ingest_parser_outputs
 from .extractor import ExtractionError, extract_pdf, extract_pdf_evidence, markitdown_version, pdf_page_text, pypdf_version
@@ -19,7 +19,7 @@ __all__ = [
     "compare_documents", "match_nodes", "normalize_text", "RawObservation", "add_observation", "observation_discrepancies", "parser_coverage",
     "observation_manifest", "observation_artifact_json", "observation_artifact_sha256",
     "from_docling_records", "from_markitdown", "from_opendataloader_records", "from_pypdf_pages", "from_records", "new_document",
-    "AdapterCapabilities", "ParserAdapter", "validate_adapter_capabilities", "RegisteredAdapter", "adapt_all", "adapt_registered", "default_adapters", "validate_default_adapters",
+    "AdapterCapabilities", "ParserAdapter", "validate_adapter_capabilities", "validate_adapter_output", "RegisteredAdapter", "adapt_all", "adapt_registered", "default_adapters", "validate_default_adapters",
     "ObservationArtifact", "ObservationPackage", "ingest_parser_outputs",
     "ExtractionError", "extract_pdf", "extract_pdf_evidence", "markitdown_version", "pdf_page_text", "pypdf_version",
     "Quality", "RecognitionAudit", "RecognitionIssue", "audit_document", "ReconciliationDecision", "ReconciliationReport", "reconcile_document",
