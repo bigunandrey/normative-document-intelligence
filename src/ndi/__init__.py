e657db3f604bdbd429bc65c35010497fc18066dd
Andrii <bigun.andrey@gmail.com>
2026-09-14 19:59:46 +0300

    feat: expose verification gates

--- src/ndi/__init__.py
+++ src/ndi/__init__.py
@@ -10,6 +10,13 @@ from .adapters import from_markitdown, from_records, new_document
 from .extractor import ExtractionError, extract_pdf, markitdown_version
 from .validator import Quality, RecognitionAudit, RecognitionIssue, audit_document
 from .reconciliation import ReconciliationDecision, ReconciliationReport, reconcile_document
+from .verification import (
+    AcceptanceEvidence, ExternalCrossCheckRecord, GateResult, GateStatus,
+    GraphicalVerificationRecord, RevisionRecord, digital_revision,
+    final_acceptance, gate_a_observations, gate_b_reconciliation,
+    gate_c_structural_acceptance, gate_d_persistence, gate_e_revision_infrastructure,
+    gate_f_verification, persist_digital_representation, write_revision_record,
+)
 
 __all__ = [
     "BoundingBox", "CanonicalDocument", "CanonicalNode", "NodeType", "ParserObservation", "SourceAnchor",
@@ -18,4 +25,9 @@ __all__ = [
     "from_markitdown", "from_records", "new_document", "ExtractionError", "extract_pdf", "markitdown_version",
     "Quality", "RecognitionAudit", "RecognitionIssue", "audit_document",
     "ReconciliationDecision", "ReconciliationReport", "reconcile_document",
+    "AcceptanceEvidence", "ExternalCrossCheckRecord", "GateResult", "GateStatus",
+    "GraphicalVerificationRecord", "RevisionRecord", "digital_revision", "final_acceptance",
+    "gate_a_observations", "gate_b_reconciliation", "gate_c_structural_acceptance",
+    "gate_d_persistence", "gate_e_revision_infrastructure", "gate_f_verification",
+    "persist_digital_representation", "write_revision_record",
 ]
