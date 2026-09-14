37a94f21b5e53ff92ad3a6b219d480e043cd7dbc
Andrii <bigun.andrey@gmail.com>
2026-09-14 21:53:36 +0300

    feat: expose external cross-check pipeline

--- src/ndi/__init__.py
+++ src/ndi/__init__.py
@@ -12,6 +12,7 @@ from .external_sources import CrossCheckResult, DiscrepancyEvidence, Discrepancy
 from .external_comparison import ExternalComparisonResult, compare_against_external, compare_discovered_sources, compare_observation_set
 from .external_retrieval import RetrievedSource, retrieve_validated, verify_retrieved_bytes
 from .external_parsing import ExternalParser, ExternalParserObservation, aggregate_external_observations, parse_retrieved_external
+from .external_pipeline import ExternalPipelineResult, run_external_cross_check
 from .extractor import ExtractionError, extract_pdf, extract_pdf_evidence, markitdown_version, pdf_page_text, pypdf_version
 from .validator import Quality, RecognitionAudit, RecognitionIssue, audit_document
 from .reconciliation import ReconciliationDecision, ReconciliationReport, reconcile_document
@@ -27,7 +28,7 @@ __all__ = [
     "ObservationArtifact", "ObservationPackage", "ingest_parser_outputs",
     "CrossCheckResult", "DiscrepancyEvidence", "DiscrepancyKind", "DiscoveryStatus", "ExternalDocument", "ExternalObservationSet", "ExternalSourceProvider", "ValidatedSource", "discover_validated", "validate_candidate",
     "ExternalComparisonResult", "compare_against_external", "compare_discovered_sources", "compare_observation_set",
-    "RetrievedSource", "retrieve_validated", "verify_retrieved_bytes", "ExternalParser", "ExternalParserObservation", "aggregate_external_observations", "parse_retrieved_external",
+    "RetrievedSource", "retrieve_validated", "verify_retrieved_bytes", "ExternalParser", "ExternalParserObservation", "aggregate_external_observations", "parse_retrieved_external", "ExternalPipelineResult", "run_external_cross_check",
     "ExtractionError", "extract_pdf", "extract_pdf_evidence", "markitdown_version", "pdf_page_text", "pypdf_version",
     "Quality", "RecognitionAudit", "RecognitionIssue", "audit_document", "ReconciliationDecision", "ReconciliationReport", "reconcile_document",
     "AcceptanceEvidence", "ExternalCrossCheckRecord", "GateResult", "GateStatus", "GraphicalVerificationRecord", "RevisionRecord",
