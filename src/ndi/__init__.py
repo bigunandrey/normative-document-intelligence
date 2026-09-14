a702fe9f6ad0e6e1d37843e5140eb523d8277886
Andrii <bigun.andrey@gmail.com>
2026-09-14 20:45:25 +0300

    feat: export multi-parser adaptation

--- src/ndi/__init__.py
+++ src/ndi/__init__.py
@@ -5,7 +5,7 @@ from .matching import compare_documents, match_nodes, normalize_text
 from .observations import RawObservation, add_observation, observation_discrepancies, parser_coverage
 from .adapters import from_docling_records, from_markitdown, from_opendataloader_records, from_pypdf_pages, from_records, new_document
 from .adapter_contract import AdapterCapabilities, ParserAdapter, validate_adapter_capabilities
-from .adapter_registry import RegisteredAdapter, adapt_registered, default_adapters, validate_default_adapters
+from .adapter_registry import RegisteredAdapter, adapt_all, adapt_registered, default_adapters, validate_default_adapters
 from .extractor import ExtractionError, extract_pdf, extract_pdf_evidence, markitdown_version, pdf_page_text, pypdf_version
 from .validator import Quality, RecognitionAudit, RecognitionIssue, audit_document
 from .reconciliation import ReconciliationDecision, ReconciliationReport, reconcile_document
@@ -16,7 +16,7 @@ __all__ = [
     "BoundingBox", "CanonicalDocument", "CanonicalNode", "NodeType", "ParserObservation", "SourceAnchor", "stable_document_id", "stable_node_id",
     "compare_documents", "match_nodes", "normalize_text", "RawObservation", "add_observation", "observation_discrepancies", "parser_coverage",
     "from_docling_records", "from_markitdown", "from_opendataloader_records", "from_pypdf_pages", "from_records", "new_document",
-    "AdapterCapabilities", "ParserAdapter", "validate_adapter_capabilities", "RegisteredAdapter", "adapt_registered", "default_adapters", "validate_default_adapters",
+    "AdapterCapabilities", "ParserAdapter", "validate_adapter_capabilities", "RegisteredAdapter", "adapt_all", "adapt_registered", "default_adapters", "validate_default_adapters",
     "ExtractionError", "extract_pdf", "extract_pdf_evidence", "markitdown_version", "pdf_page_text", "pypdf_version",
     "Quality", "RecognitionAudit", "RecognitionIssue", "audit_document", "ReconciliationDecision", "ReconciliationReport", "reconcile_document",
     "AcceptanceEvidence", "ExternalCrossCheckRecord", "GateResult", "GateStatus", "GraphicalVerificationRecord", "RevisionRecord",
