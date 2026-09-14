b71f46f3af1db00205bae71fdfec5112476c58d2
Andrii <bigun.andrey@gmail.com>
2026-09-14 21:59:29 +0300

    feat: export reconciliation resolution contracts

--- src/ndi/__init__.py
+++ src/ndi/__init__.py
@@ -13,7 +13,7 @@ from .external_comparison import ExternalComparisonResult, compare_against_exter
 from .external_retrieval import RetrievedSource, retrieve_validated, verify_retrieved_bytes
 from .external_parsing import ExternalParser, ExternalParserObservation, aggregate_external_observations, parse_retrieved_external
 from .external_pipeline import ExternalPipelineResult, run_external_cross_check
-from .integrated_reconciliation import IntegratedReconciliationReport, reconcile_with_external
+from .integrated_reconciliation import IntegratedReconciliationReport, ResolutionAction, ResolutionDecision, ResolvedReconciliation, reconcile_with_external, resolve_reconciliation
 from .extractor import ExtractionError, extract_pdf, extract_pdf_evidence, markitdown_version, pdf_page_text, pypdf_version
 from .validator import Quality, RecognitionAudit, RecognitionIssue, audit_document
 from .reconciliation import ReconciliationDecision, ReconciliationReport, reconcile_document
@@ -30,7 +30,7 @@ __all__ = [
     "CrossCheckResult", "DiscrepancyEvidence", "DiscrepancyKind", "DiscoveryStatus", "ExternalDocument", "ExternalObservationSet", "ExternalSourceProvider", "ValidatedSource", "discover_validated", "validate_candidate",
     "ExternalComparisonResult", "compare_against_external", "compare_discovered_sources", "compare_observation_set",
     "RetrievedSource", "retrieve_validated", "verify_retrieved_bytes", "ExternalParser", "ExternalParserObservation", "aggregate_external_observations", "parse_retrieved_external", "ExternalPipelineResult", "run_external_cross_check",
-    "IntegratedReconciliationReport", "reconcile_with_external",
+    "IntegratedReconciliationReport", "ResolutionAction", "ResolutionDecision", "ResolvedReconciliation", "reconcile_with_external", "resolve_reconciliation",
     "ExtractionError", "extract_pdf", "extract_pdf_evidence", "markitdown_version", "pdf_page_text", "pypdf_version",
     "Quality", "RecognitionAudit", "RecognitionIssue", "audit_document", "ReconciliationDecision", "ReconciliationReport", "reconcile_document",
     "AcceptanceEvidence", "ExternalCrossCheckRecord", "GateResult", "GateStatus", "GraphicalVerificationRecord", "RevisionRecord",
