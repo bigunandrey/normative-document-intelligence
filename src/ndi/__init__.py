aac9488f750626ea3c5327cd4c4ad859270a54f7
Andrii <bigun.andrey@gmail.com>
2026-09-14 18:25:32 +0300

    feat: expose reconciliation API

--- src/ndi/__init__.py
+++ src/ndi/__init__.py
@@ -9,6 +9,7 @@ from .observations import RawObservation, add_observation, observation_discrepan
 from .adapters import from_markitdown, from_records, new_document
 from .extractor import ExtractionError, extract_pdf, markitdown_version
 from .validator import Quality, RecognitionAudit, RecognitionIssue, audit_document
+from .reconciliation import ReconciliationDecision, ReconciliationReport, reconcile_document
 
 __all__ = [
     "BoundingBox", "CanonicalDocument", "CanonicalNode", "NodeType", "ParserObservation", "SourceAnchor",
@@ -16,4 +17,5 @@ __all__ = [
     "RawObservation", "add_observation", "observation_discrepancies", "parser_coverage",
     "from_markitdown", "from_records", "new_document", "ExtractionError", "extract_pdf", "markitdown_version",
     "Quality", "RecognitionAudit", "RecognitionIssue", "audit_document",
+    "ReconciliationDecision", "ReconciliationReport", "reconcile_document",
 ]
