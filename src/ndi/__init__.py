ac1560891cf5f744e03e9b9fc561ca0d092b7816
Andrii <bigun.andrey@gmail.com>
2026-09-14 22:22:16 +0300

    feat: export revision lock API

--- src/ndi/__init__.py
+++ src/ndi/__init__.py
@@ -16,6 +16,7 @@ from .external_pipeline import ExternalPipelineResult, run_external_cross_check
 from .integrated_reconciliation import IntegratedReconciliationReport, ResolutionAction, ResolutionDecision, ResolvedReconciliation, reconcile_with_external, resolve_reconciliation
 from .dbn_gate import DBNFixtureEvidence, EvidenceMode, build_dbn_fixture_evidence, dbn_structural_gate, write_dbn_fixture_evidence
 from .graphical_verification import GraphicalVerificationScope, validate_graphical_verification
+from .revision_lock import PROTOCOL_VERSION, RevisionLock, build_revision_lock, persist_revision_lock, verify_revision_lock
 from .extractor import ExtractionError, extract_pdf, extract_pdf_evidence, markitdown_version, pdf_page_text, pypdf_version
 from .validator import Quality, RecognitionAudit, RecognitionIssue, audit_document
 from .reconciliation import ReconciliationDecision, ReconciliationReport, reconcile_document
@@ -34,7 +35,7 @@ __all__ = [
     "RetrievedSource", "retrieve_validated", "verify_retrieved_bytes", "ExternalParser", "ExternalParserObservation", "aggregate_external_observations", "parse_retrieved_external", "ExternalPipelineResult", "run_external_cross_check",
     "IntegratedReconciliationReport", "ResolutionAction", "ResolutionDecision", "ResolvedReconciliation", "reconcile_with_external", "resolve_reconciliation",
     "DBNFixtureEvidence", "EvidenceMode", "build_dbn_fixture_evidence", "dbn_structural_gate", "write_dbn_fixture_evidence",
-    "GraphicalVerificationScope", "validate_graphical_verification",
+    "GraphicalVerificationScope", "validate_graphical_verification", "PROTOCOL_VERSION", "RevisionLock", "build_revision_lock", "persist_revision_lock", "verify_revision_lock",
     "ExtractionError", "extract_pdf", "extract_pdf_evidence", "markitdown_version", "pdf_page_text", "pypdf_version",
     "Quality", "RecognitionAudit", "RecognitionIssue", "audit_document", "ReconciliationDecision", "ReconciliationReport", "reconcile_document",
     "AcceptanceEvidence", "ExternalCrossCheckRecord", "GateResult", "GateStatus", "GraphicalVerificationRecord", "RevisionRecord",
