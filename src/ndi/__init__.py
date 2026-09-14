7919ea2c341f60d6c12bd93eb9a9b86d2b9e747c
Andrii <bigun.andrey@gmail.com>
2026-09-14 22:05:07 +0300

    feat: export DBN structural gate

--- src/ndi/__init__.py
+++ src/ndi/__init__.py
@@ -14,6 +14,7 @@ from .external_retrieval import RetrievedSource, retrieve_validated, verify_retr
 from .external_parsing import ExternalParser, ExternalParserObservation, aggregate_external_observations, parse_retrieved_external
 from .external_pipeline import ExternalPipelineResult, run_external_cross_check
 from .integrated_reconciliation import IntegratedReconciliationReport, ResolutionAction, ResolutionDecision, ResolvedReconciliation, reconcile_with_external, resolve_reconciliation
+from .dbn_gate import DBNFixtureEvidence, EvidenceMode, build_dbn_fixture_evidence, dbn_structural_gate, write_dbn_fixture_evidence
 from .extractor import ExtractionError, extract_pdf, extract_pdf_evidence, markitdown_version, pdf_page_text, pypdf_version
 from .validator import Quality, RecognitionAudit, RecognitionIssue, audit_document
 from .reconciliation import ReconciliationDecision, ReconciliationReport, reconcile_document
@@ -31,6 +32,7 @@ __all__ = [
     "ExternalComparisonResult", "compare_against_external", "compare_discovered_sources", "compare_observation_set",
     "RetrievedSource", "retrieve_validated", "verify_retrieved_bytes", "ExternalParser", "ExternalParserObservation", "aggregate_external_observations", "parse_retrieved_external", "ExternalPipelineResult", "run_external_cross_check",
     "IntegratedReconciliationReport", "ResolutionAction", "ResolutionDecision", "ResolvedReconciliation", "reconcile_with_external", "resolve_reconciliation",
+    "DBNFixtureEvidence", "EvidenceMode", "build_dbn_fixture_evidence", "dbn_structural_gate", "write_dbn_fixture_evidence",
     "ExtractionError", "extract_pdf", "extract_pdf_evidence", "markitdown_version", "pdf_page_text", "pypdf_version",
     "Quality", "RecognitionAudit", "RecognitionIssue", "audit_document", "ReconciliationDecision", "ReconciliationReport", "reconcile_document",
     "AcceptanceEvidence", "ExternalCrossCheckRecord", "GateResult", "GateStatus", "GraphicalVerificationRecord", "RevisionRecord",
