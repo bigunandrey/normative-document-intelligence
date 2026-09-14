1f6e2c6c02d8ff8cf600890882812dcf6e9ac9dc
Andrii <bigun.andrey@gmail.com>
2026-09-14 21:13:08 +0300

    feat: export observation artifact manifest

--- src/ndi/__init__.py
+++ src/ndi/__init__.py
@@ -3,6 +3,7 @@
 from .canonical import BoundingBox, CanonicalDocument, CanonicalNode, NodeType, ParserObservation, SourceAnchor, stable_document_id, stable_node_id
 from .matching import compare_documents, match_nodes, normalize_text
 from .observations import RawObservation, add_observation, observation_discrepancies, parser_coverage
+from .observation_artifacts import observation_artifact_json, observation_artifact_sha256, observation_manifest
 from .adapters import from_docling_records, from_markitdown, from_opendataloader_records, from_pypdf_pages, from_records, new_document
 from .adapter_contract import AdapterCapabilities, ParserAdapter, validate_adapter_capabilities
 from .adapter_registry import RegisteredAdapter, adapt_all, adapt_registered, default_adapters, validate_default_adapters
@@ -15,6 +16,7 @@ from .source_registry import Compatibility, DocumentIdentity, SourceCandidate, S
 __all__ = [
     "BoundingBox", "CanonicalDocument", "CanonicalNode", "NodeType", "ParserObservation", "SourceAnchor", "stable_document_id", "stable_node_id",
     "compare_documents", "match_nodes", "normalize_text", "RawObservation", "add_observation", "observation_discrepancies", "parser_coverage",
+    "observation_manifest", "observation_artifact_json", "observation_artifact_sha256",
     "from_docling_records", "from_markitdown", "from_opendataloader_records", "from_pypdf_pages", "from_records", "new_document",
     "AdapterCapabilities", "ParserAdapter", "validate_adapter_capabilities", "RegisteredAdapter", "adapt_all", "adapt_registered", "default_adapters", "validate_default_adapters",
     "ExtractionError", "extract_pdf", "extract_pdf_evidence", "markitdown_version", "pdf_page_text", "pypdf_version",
