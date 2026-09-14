d25d60e9c1a8c61b86d763ee6863df5f3210f0a4
Andrii <bigun.andrey@gmail.com>
2026-09-14 21:40:27 +0300

    feat: export fail-closed external retrieval

--- src/ndi/__init__.py
+++ src/ndi/__init__.py
@@ -10,6 +10,7 @@ from .adapter_registry import RegisteredAdapter, adapt_all, adapt_registered, de
 from .ingestion import ObservationArtifact, ObservationPackage, ingest_parser_outputs
 from .external_sources import CrossCheckResult, DiscrepancyEvidence, DiscrepancyKind, DiscoveryStatus, ExternalDocument, ExternalObservationSet, ExternalSourceProvider, ValidatedSource, discover_validated, validate_candidate
 from .external_comparison import ExternalComparisonResult, compare_against_external, compare_discovered_sources
+from .external_retrieval import RetrievedSource, retrieve_validated, verify_retrieved_bytes
 from .extractor import ExtractionError, extract_pdf, extract_pdf_evidence, markitdown_version, pdf_page_text, pypdf_version
 from .validator import Quality, RecognitionAudit, RecognitionIssue, audit_document
 from .reconciliation import ReconciliationDecision, ReconciliationReport, reconcile_document
@@ -25,6 +26,7 @@ __all__ = [
     "ObservationArtifact", "ObservationPackage", "ingest_parser_outputs",
     "CrossCheckResult", "DiscrepancyEvidence", "DiscrepancyKind", "DiscoveryStatus", "ExternalDocument", "ExternalObservationSet", "ExternalSourceProvider", "ValidatedSource", "discover_validated", "validate_candidate",
     "ExternalComparisonResult", "compare_against_external", "compare_discovered_sources",
+    "RetrievedSource", "retrieve_validated", "verify_retrieved_bytes",
     "ExtractionError", "extract_pdf", "extract_pdf_evidence", "markitdown_version", "pdf_page_text", "pypdf_version",
     "Quality", "RecognitionAudit", "RecognitionIssue", "audit_document", "ReconciliationDecision", "ReconciliationReport", "reconcile_document",
     "AcceptanceEvidence", "ExternalCrossCheckRecord", "GateResult", "GateStatus", "GraphicalVerificationRecord", "RevisionRecord",
